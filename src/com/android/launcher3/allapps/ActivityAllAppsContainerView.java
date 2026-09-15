c1f3e734cda994390d595374fac3c4031b84cb86
SoloSaravanan <SoloSaravanan@proton.me>
2026-09-15 08:14:18 +0000

    Launcher3: use AMOLED black surfaces and themed icons

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -353,9 +353,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
         int layerFg = getContext().getColor(R.color.blur_shade_panel_fg);
         int layerBg = getContext().getColor(R.color.blur_shade_panel_bg);
         mBottomSheetBackgroundColorOverBlur = ColorUtils.compositeColors(layerFg, layerBg);
-        mBottomSheetBackgroundColorBlurFallback = getContext().getColor(
-                Utilities.isDarkTheme(getContext()) ? android.R.color.system_accent2_800
-                        : android.R.color.system_accent2_200);
+        mBottomSheetBackgroundColorBlurFallback = Color.BLACK;
 
         mSearchUiManager.initializeSearch(this);
         updateOneUiPageIndicatorState();
